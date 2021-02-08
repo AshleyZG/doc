a92e77a366a02541e3a50461e6545820d8632cdd
Ge Zhang <gsarazh@gmail.com>
2021-02-08 21:46:35 +0800

    new title

--- src/demo/collaborative-notebook.js
+++ src/demo/collaborative-notebook.js
@@ -7,9 +7,8 @@ function CollaborativeNotebookDemo (){
     return (
         <div>
             <IRBTitle text="Introduction"></IRBTitle>
-            {/* <p>hi</p> */}
             <IRBTitle text="How to use it"></IRBTitle>
-            <IRBTitle text="Get started"></IRBTitle>
+            <IRBTitle text="Get it work locally"></IRBTitle>
         </div>
     )
 };
