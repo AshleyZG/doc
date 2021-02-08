2d1e37f9d5aa577fbcf2e7cb24cd06b7f8a64df4
Ge Zhang <gsarazh@gmail.com>
2021-02-09 05:37:02 +0800

    collaborative notebook demo: how to use it

--- src/demo/collaborative-notebook.js
+++ src/demo/collaborative-notebook.js
@@ -2,12 +2,25 @@ import React from 'react'
 import {IRBTitle, IRBSubTitle, IRBContent} from '../components/IRBElements';
 import { Divider } from '@material-ui/core';
 
+const introduction = "This is a demo for Jupyter Notebook branching. It supports real time collaboration, modularization, and branching in JupyterLab."
+const howToUseIt = ""
 
 function CollaborativeNotebookDemo (){
     return (
         <div>
             <IRBTitle text="Introduction"></IRBTitle>
+            <IRBContent text={introduction}></IRBContent>
+            <Divider/>
             <IRBTitle text="How to use it"></IRBTitle>
+            <p className="irb-content">
+                Real time collaboration: <br/>
+                <a href="http://3.22.120.178:8822">http://3.22.120.178:8822</a>
+            </p>
+            <p className="irb-content">
+                Extensions: <br/>
+
+            </p>
+            <IRBContent text={howToUseIt}></IRBContent>
             <IRBTitle text="Get it work locally"></IRBTitle>
         </div>
     )
